58dd92156a0a989d632202f80b0343d830156d72
Rachael Legge <legge_rachael@student.smc.edu>
2020-04-18 22:23:59 -0700

    Commit 5
    Added team, finished about page. Attempted modal.

--- MIDTERM/script.js
+++ MIDTERM/script.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //we write everything here
 
     $('#myCarousel').carousel({
         interval: 2000
     });
 
-});
+    $("form.needs-validation").submit(function (event) {
+        event.preventDefault();
 
-   
+        if ($("input#exampleInputEmail1").val() === "") {
+            $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
+        }
+    });
+
+    $('#modalLauncher').click(function(){
+        $('#exampleModal').modal({
+            backdrop: false
+        });
+    });
+
+});
